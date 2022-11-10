d46fee90e07ac2a2bc113e60ff38c660c7e4924c
Eleazar Gamez <82742317+froitzent@users.noreply.github.com>
2022-11-10 14:47:28 -0500

    agregando funcion de Json

--- web2.0/main.js
+++ web2.0/main.js
@@ -1,4 +1,9 @@
-const todos = [];
+const actualizaSalvardato = salvardato => {
+  const salvardatoStrings = JSON.stringify (salvardato);
+  localStorage.setItem ('salvardato', salvardatoStrings);
+};
+
+const todos = JSON.parse (localStorage.getItem ('todos')) || [];
 const render = () => {
   const todolist = document.getElementById ('todo-list');
   const todosTemplate = todos.map (t => '<li>' + t + '</li>');
@@ -9,18 +14,21 @@ const render = () => {
     elemento.addEventListener ('click', () => {
       elemento.parentNode.removeChild (elemento);
       todos.splice (i, 1);
+      actualizaSalvardato (salvardato);
+      render ();
     });
   });
 };
-
-const form = document.getElementById ('listado');
-form.onsubmit = e => {
-  e.preventDefault ();
-  const salvar = document.getElementById ('texto');
-  const salvardato = salvar.value;
-  salvar.value = ' ';
-  console.log (salvardato);
-
-  todos.push (salvardato);
+Window.onload = () => {
   render ();
+  const form = document.getElementById ('listado');
+  form.onsubmit = e => {
+    e.preventDefault ();
+    const salvar = document.getElementById ('texto');
+    const salvardato = salvar.value;
+    salvar.value = ' ';
+    console.log (salvardato);
+    todos.push (salvardato);
+    actualizaSalvardato (salvardato);
+  };
 };
