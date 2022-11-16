1fdac58604cb07e6a44aae25ba688c0f0df2cb57
Eleazar Gamez <82742317+froitzent@users.noreply.github.com>
2022-11-16 10:57:31 -0500

    agregada funcion del boton eliminar

--- api/app/main.js
+++ api/app/main.js
@@ -20,9 +20,16 @@ const getUsers = async () => {
   const users = await response.json ();
   console.log (users);
   const template = user =>
-    `<li>${user.name} ${user.lastname}<button data-id="${user._id}">Eliminar</button></button></li>`;
+    `<li>${user.name} ${user.lastname}<button data-id="${user._id}">Eliminar</button></li>`;
   const userList = document.getElementById ('user-list');
   userList.innerHTML = users.map (user => template (user)).join ('');
+  users.forEach (user => {
+    const userNode = document.querySelector (`[data-id="${user._id}"]`);
+    userNode.onclick = async e => {
+      await fetch (`/users/${user._id}`, {method: 'DELETE'});
+      userNode.parentNode.remove ();
+    };
+  });
 };
 const addFormListener = () => {
   const userForm = document.getElementById ('user-form');
