556a38582e2f7c674f20d36dcefdc94aab6266e4
Eleazar Gamez <82742317+froitzent@users.noreply.github.com>
2022-12-05 17:11:06 -0500

    clase REACT

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,9 +1,16 @@
 import Button from './Button';
 
+const arr = ['c1', 'c2', 'c3'];
+
 const App = () => {
+  const miVariable = false;
+  if (miVariable) {
+    return <p>Mi Variable es TRUE </p>;
+  }
   return (
     <div>
       <h1 onClick={e => console.log ('Click', e)}>Hola Mundo</h1>
+      {arr.map (el => <p key={el}>{el}</p>)} 
       <Button onClick={() => console.log ('clickeado')}>Enviar</Button>
     </div>
   );
