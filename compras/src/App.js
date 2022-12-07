c2eb82ef92a31d5facb5d1e29c4eaf77a896b984
ELEAZAR GAMEZ <82742317+froitzent@users.noreply.github.com>
2022-12-06 22:46:11 -0500

    agregando componentes , fallo del .map

--- compras/src/App.js
+++ compras/src/App.js
@@ -1,15 +1,24 @@
 import {Component} from 'react';
-import Productos from './components/productos';
+import Productos from './components/Productos';
 
 class App extends Component {
   state = {
     productos: [
-      {nombre: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
-      {nombre: 'Caraotas', price: 500, img: '/productos/caraotas.jpg'},
-      {nombre: 'Harina Pan', price: 5000, img: '/productos/harina.jpg'},
-      {nombre: 'Lechuga', price: 570, img: '/productos/lechuga.jpg'},
+      {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
+      {name: 'Caraotas', price: 500, img: '/productos/caraotas.jpg'},
+      {name: 'Harina Pan', price: 5000, img: '/productos/harina.jpg'},
+      {name: 'Lechuga', price: 570, img: '/productos/lechuga.jpg'},
     ],
   };
+  render () {
+    return (
+      <div>
+        <Productos
+          agregarAlCarro={() => console.log ('todavia no hacemos nada')}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- compras/src/components/Productos.js
+++ compras/src/components/Productos.js
@@ -1,12 +1,12 @@
 import {Component} from 'react';
-
+import Producto from './Producto';
 class Productos extends Component {
   render () {
     const {productos, agregarAlCarro} = this.props;
     return (
       <div>
         {productos.map (producto => (
-          <producto
+          <Producto
             agregarAlCarro={agregarAlCarro}
             key={producto.name}
             producto={producto}
